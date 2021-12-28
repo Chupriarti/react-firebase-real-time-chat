187cc04188c94a04a81a8be346ab0d70a2dbcca9
Chupriarti <wallx13@gmail.com>
2021-12-28 22:54:11 +0300

    prepare Login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,17 @@
+import { Container, Grid } from '@material-ui/core';
 import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Grid container
+                style={{height: window.innerHeight - 50}}
+                alignItems={"center"}
+                justifyContent={"center"}
+            >
+                Login
+            </Grid>
+        </Container>
     )
 }
 
