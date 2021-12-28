c52a6895a39ae8a0af94ce12bd24fc44c7edf5a6
Chupriarti <wallx13@gmail.com>
2021-12-28 22:46:22 +0300

    add navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Button, Grid, Toolbar } from '@material-ui/core';
 import React from 'react';
 
 const Navbar = () => {
     return (
         <AppBar color={"secondary"} position="static">
             <Toolbar>
-
+                <Grid container justify={"flex-end"}>
+                    <Button variant={"outlined"}>Login</Button>
+                    <Button variant={"outlined"}>Logout</Button>
+                </Grid>
             </Toolbar>
         </AppBar>
     )
